aeb7fc49ffac6c9dc2421f67c06a010b42a1d50b
gleonio <gleonio@gmail.com>
2025-03-21 17:38:27 -0700

    created footer

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,12 +16,11 @@ const About = () => {
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
-          <span className="about__paragraph-seperator">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </span>
+          <span className="about__paragraph-seperator"></span>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
         </p>
       </div>
     </section>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,33 @@
+// import { Link } from "react-router-dom";
+import githhubIcon from "../../assets/github.png";
+import facebookIcon from "../../assets/facebook.svg";
 import "../Footer/Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <p className="footer__copy">© 2025 Supersite, Powered by News API</p>
       <div className="footer__container">
-        <p className="footer__text">© 2023 Your Company Name</p>
+        <div className="footer__container-links">
+          <p className="footer__link">Home</p>
+          <a
+            className="footer__link"
+            href="https://www.linkedin.com/in/gleonio"
+          >
+            G. Leonio
+          </a>
+        </div>
+        <div className="footer__container-icons">
+          <a
+            className="footer__icon-link"
+            href="https://github.com/gcleonio/news_explorer"
+          >
+            <img src={githhubIcon} alt="github link"></img>
+          </a>
+          <a className="footer__icon-link" href="https://www.facebook.com/">
+            <img src={facebookIcon} alt="facebook"></img>
+          </a>
+        </div>
       </div>
     </footer>
   );
